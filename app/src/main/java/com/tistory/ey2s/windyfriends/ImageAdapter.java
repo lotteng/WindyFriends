debbd75ce2fd6c49c6c9a14aa00194e5afd60755
lotteng <lotteng_@naver.com>
2018-01-18 19:47:22 +0900

    MakeActivity/sample grid'UI/UX'

---
+++ app/src/main/java/com/tistory/ey2s/windyfriends/ImageAdapter.java
@@ -0,0 +1,61 @@
+package com.tistory.ey2s.windyfriends;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by 151120 on 2018-01-15.
+ */
+
+public class ImageAdapter extends BaseAdapter{
+
+    private Context mContext;
+
+    private Integer[] mThumbIds = {
+            R.drawable.red, R.drawable.yellow
+    };
+
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if(convertView == null){
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85,85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        }else{
+            imageView = (ImageView)convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+
+        return imageView;
+    }
+
+
+
+}
--- app/src/main/java/com/tistory/ey2s/windyfriends/MakeActivity.java
+++ app/src/main/java/com/tistory/ey2s/windyfriends/MakeActivity.java
@@ -1,10 +1,32 @@
 package com.tistory.ey2s.windyfriends;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 /**
  * Created by 151120 on 2018-01-15.
  */
 
 public class MakeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make);
+
+        GridView gridView = (GridView)findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(this));
+
+    }
+
+
+
+
+
 }
